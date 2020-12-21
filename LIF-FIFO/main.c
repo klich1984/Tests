b51e0ae13f39a4f828e6c444477a1d2b56407660
klich1984 <2239@holbertonschool.com>
2020-12-21 19:06:14 +0000

    05 | code fix | dividing the line with the shell function

--- LIF-FIFO/main.c
+++ LIF-FIFO/main.c
@@ -2,30 +2,29 @@
 
 void opcode_push(__attribute__((unused))stack_t **head_list, unsigned int line_number)
 {
-	printf("line: %d\n", line_number);
+	printf("funcion opcode_push line: %d\n", line_number);
 }
 
 void opcode_pall(__attribute__((unused))stack_t **head_list, unsigned int line_number)
 {
-	printf("line_pall: %d\n", line_number);
+	printf("funcion opcode_pall  line_pall: %d\n", line_number);
 }
 
 /*Funcion que busca y devuelve un pointer a la funcion*/
 void get_opcode_function(char *opcode_input, stack_t **head_list, unsigned int line_number)
 {
 	instruction_t function[] = {
-	    {"ush", opcode_push},
-	    {"pall", opcode_pall},
-	    {NULL, NULL}
-		};
+		{"push", opcode_push},
+		{"pall", opcode_pall},
+		{NULL, NULL}
+	};
 	int i = 0;
 
 	/* find the input function in our opcode function */
 	for (; function[i].opcode != NULL; i++)
-	{
-		if (*(function[i].opcode) == *opcode_input);
-			break;
-	}
+		;
+	if (function[i].opcode == NULL)
+		return;
 	function[i].f(head_list, line_number);
 }
 
@@ -43,7 +42,6 @@ char *new_line_remove(char *buffer)
 /*main*/
 int main(int argc, char **argv)
 {
-
 	if (argc > 2 || argc == 0)
 	{
 		printf("USAGE: monty file");
@@ -59,15 +57,21 @@ int main(int argc, char **argv)
 	char *buffer = NULL, *opcode = NULL;
 	size_t bytes_qty = 0;
 	stack_t *head_list = NULL;
-	int line_number = 1;
+	int line_number = 1, value = 0;
+	char **tokens = NULL;
 
 	file_pointer = fopen(*(argv + 1), "r");
 	for (; getline(&buffer, &bytes_qty, file_pointer) != EOF; line_number++)
 	{
 		/* remove new line */
 		buffer = new_line_remove(buffer);
+		printf("buffer = %s\n", buffer);
+		tokens = divide_line(buffer, " ");
+		printf("tokens[0] = %s | tokens[1] = %s\n", tokens[0], tokens[1]);
 		/* get opcode = pall or push */
 		opcode = strtok(buffer, " ");
+		//value = atoi(strtok(NULL, " "));
+		printf("line = %d | opcode = %s | value = %d\n", line_number, opcode, value);
 		/* get the opcode, no need to return */
 		get_opcode_function(opcode, &head_list, line_number);
 		/* *data = NULL ??????????? data = strtok(NULL, " , \n");
