d33c23a131390b18162624607f02cf4fb06a7770
klich1984 <2239@holbertonschool.com>
2020-12-22 02:03:03 +0000

    09 | code fix | implementation of structure for variables

--- LIF-FIFO/main.c
+++ LIF-FIFO/main.c
@@ -1,74 +1,5 @@
 #include "monty.h"
 
-void opcode_push(__attribute__((unused))stack_t **head_list, unsigned int line_number)
-{
-	printf("funcion opcode_push line: %d\n", line_number);
-}
-
-void opcode_pall(__attribute__((unused))stack_t **head_list, unsigned int line_number)
-{
-	printf("funcion opcode_pall  line_pall: %d\n", line_number);
-}
-
-/*Funcion que busca y devuelve un pointer a la funcion*/
-void get_opcode_function(char *opcode_input, stack_t **head_list, unsigned int line_number)
-{
-	instruction_t function[] = {
-		{"push", opcode_push},
-		{"pall", opcode_pall},
-		{NULL, NULL}
-	};
-	int i = 0, len_str = 0;
-	char *str = NULL;
-
-	while (function[i].opcode != NULL)
-	{
-		str = function[i].opcode;
-		if (strcmp(opcode_input, str) == 0)
-		{
-			printf("Las palabras son identicas\n");
-			function[i].f(head_list, line_number);
-			return;
-		}
-		i++;
-	}
-
-	/*len_str = strlen(opcode_input);
-	printf("len_str = %d\n", len_str);
-	for (i = 0; opcode_input[i] == function->opcode[i] && opcode_input[i] != '\0'; i++)
-	{
-		printf("%c", opcode_input[i]);
-	}
-	printf("\n");
-	printf("i = %d\n", i);
-	if (i == len_str)
-		function[1].f(head_list, line_number);
-		//printf("son iguales\n");
-	else
-		printf("No son iguales\n");
-
-
-
-
-	/* find the input function in our opcode function */
-	/*for (; function[i].opcode != NULL; i++)
-		;
-	if (function[i].opcode == NULL)
-		return;
-	function[i].f(head_list, line_number);*/
-}
-
-/*elimina \n*/
-char *new_line_remove(char *buffer)
-{
-	int i = 0;
-
-	for (; *(buffer + i) != '\n'; i++)
-		;
-	*(buffer + i) = '\0';
-	return (buffer);
-}
-
 /*main*/
 int main(int argc, char **argv)
 {
@@ -83,21 +14,24 @@ int main(int argc, char **argv)
 		return (EXIT_FAILURE);
 	}
 
-	FILE *file_pointer = NULL;
-	char *buffer = NULL, *opcode = NULL, *str = "push", *token_1 = NULL;
-	size_t bytes_qty = 0;
-	stack_t *head_list = NULL;
-	int line_number = 1, value = 0, i = 0;
-	char **tokens = NULL;
+	//FILE *file_pointer = NULL;
+	//char *buffer = NULL;
+	char *opcode = NULL, *token_1 = NULL;
+	//size_t bytes_qty = 0;
+	//stack_t *head_list = NULL;
+	//int line_number = 1;
+	int value = 0;
+	var_t.line_number = 1;
 
-	file_pointer = fopen(*(argv + 1), "r");
-	for (; getline(&buffer, &bytes_qty, file_pointer) != EOF; line_number++)
+	/*Abro el archivo*/
+	var_t.file_pointer = fopen(*(argv + 1), "r");
+	/*Asigno tokens a variables y llamo la funcion que busca funciones*/
+	for (; getline(&var_t.buffer, &var_t.bytes_qty, var_t.file_pointer) != EOF; var_t.line_number++)
 	{
 		/* remove new line */
-		buffer = new_line_remove(buffer);
-		printf("buffer = %s\n", buffer);
-		//tokens = divide_line(buffer, " ");
-		opcode = strtok(buffer, " ");
+		var_t.buffer = new_line_remove(var_t.buffer);
+		printf("buffer = %s\n", var_t.buffer);
+		opcode = strtok(var_t.buffer, " ");
 		printf("opcode %s\n", opcode);
 		token_1 = strtok(NULL, " ");
 		if (token_1)
@@ -105,22 +39,13 @@ int main(int argc, char **argv)
 		else
 			value = 0;
 		printf("value %d\n", value);
-		/*opcode = tokens[0];
-		if(tokens[1])
-			value = atoi(tokens[1]);
-		else
-			value = 0;*/
-
-		//printf("tokens[0] = %s | opcode = %s\n", tokens[0], opcode);
-		//printf("tokens[1] = %s | value = %d\n", tokens[1], value);
-
-		printf("line = %d | opcode = %s | value = %d\n", line_number, opcode, value);
+		printf("line = %d | opcode = %s | value = %d\n", var_t.line_number, opcode, value);
 		/* get the opcode, no need to return */
-		get_opcode_function(opcode, &head_list, line_number);
+		get_opcode_function(opcode, &var_t.head_list, var_t.line_number);
 		/* *data = NULL ??????????? data = strtok(NULL, " , \n");
 		printf("%s\n", data); */
 	}
-	fclose(file_pointer);
-	free(buffer);
+	fclose(var_t.file_pointer);
+	free(var_t.buffer);
 	return (0);
 }
