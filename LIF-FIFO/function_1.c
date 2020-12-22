3a0e5dae7b15637fd1e388c938291a581556e381
klich1984 <2239@holbertonschool.com>
2020-12-22 03:29:16 +0000

    01 | update code | implementation of push and free

--- LIF-FIFO/function_1.c
+++ LIF-FIFO/function_1.c
@@ -1,78 +1,103 @@
 #include "monty.h"
 
-/**
-* divide_line - divides a line
-* @line: line to be divided
-* @delim: delimitator
-* Return: array of arguments
-*/
-
-char **divide_line(char *line, char *delim)
+/*elimina \n*/
+char *new_line_remove(char *buffer)
 {
-	char **tokens = NULL;
-	int i = 0, j = 0, coun_tokens = 0;
+	int i = 0;
 
-	coun_tokens = _count_token(line, delim);
-	if (coun_tokens == 0 || !line)
-		return (NULL);
-	tokens = malloc(sizeof(char *) * (coun_tokens + 1));
-	if (tokens == NULL)
-		return (NULL);
-	while (line[i] != '\0')
+	for (; *(buffer + i) != '\n'; i++)
+		;
+	*(buffer + i) = '\0';
+	return (buffer);
+}
+
+/*Funcion que busca y devuelve un pointer a la funcion*/
+void get_opcode_function(char *opcode_input, stack_t **head_list, unsigned int line_number)
+{
+	instruction_t function[] = {
+		{"push", opcode_push},
+		{"pall", opcode_pall},
+		{NULL, NULL}
+	};
+	int i = 0;
+	char *str = NULL;
+
+	while (function[i].opcode != NULL)
 	{
-		if (i == 0 && line[i] == ' ')
+		str = function[i].opcode;
+		/*Si es = 0 las palabras son identicas*/
+		if (strcmp(opcode_input, str) == 0)
 		{
-			while (line[i + 1] == ' ' && j == 0)
-				i++;
-			i++;
-			tokens[j] = line + i;
-			j++;
-		}
-		if (i == 0 && line[i] != ' ')
-		{
-			tokens[j] = line;
-			j++;
-		}
-		while (line[i + 1] == *delim || line[i + 1] == ' ')
-		{
-			i++;
-			line[i] = '\0';
-			while (line[i + 1] == ' ')
-			{
-				i++;
-				line[i] = '\0';
-			}
-			i++;
-			tokens[j++] = line + i;
+			function[i].f(head_list, line_number);
+			return;
 		}
 		i++;
 	}
-	tokens[coun_tokens] = NULL;
-	return (tokens);
+}
+
+void opcode_push(stack_t **head_list, unsigned int line_number)
+{
+	stack_t *new = NULL;
+	stack_t *temp = NULL;
+	char *token = NULL;
+	int value = 0;
+
+	token = strtok(NULL, " ");
+	if (token)
+		value = atoi(token);
+	else
+		value = 0;
+	printf("value %d\n", value);
+	new = (malloc(sizeof(stack_t)));
+	if (new == NULL)
+	{
+		dprintf(2, "Error: Can't malloc\n");
+		return;
+	}
+	new->prev = NULL;
+	new->next = *head_list;
+	new->n = value;
+	if (*head_list == NULL)
+		*head_list = new;
+	else
+	{
+		temp = *head_list;
+		*head_list = new;
+		temp->prev = new;
+	}
+	/*return (new);*/
+	printf("funcion opcode_push line: %d\n", line_number);
+}
+
+void opcode_pall(__attribute__((unused))stack_t **head_list, unsigned int line_number)
+{
+	printf("funcion opcode_pall line_pall: %d\n", line_number);
 }
 
 /**
- *  _count_token - divides a line
- *  @line: line to be divided
- *  @delim: delimitator
- *
- *  Return: array of arguments
- */
-int _count_token(char *line, char *delim)
-{
-	int coun_tokens = 0, i = 0;
+* free_dlistint - function that free double link list
+* @head: pointer a double linked list
+* Return: Nothing
+*/
 
-	while (line[i] != '\0')
+void free_dlistint(void)
+{
+	stack_t *free_list = var_t.head_list;
+	if (free_list == NULL)
+		return;
+
+	if (free_list->prev != NULL)
 	{
-		if (line[i] == ' ')
-			i++;
-		else
+		while (free_list->prev != NULL)
+			free_list = free_list->prev;
+	}
+	else
+	{
+		while (free_list->next != NULL)
 		{
-			if (line[i + 1] == ' ' || line[i + 1] == '\0'
-			|| line[i + 1] == *delim)
-				coun_tokens++;
-			i++;
+			free_list = free_list->next;
+			free(free_list->prev);
 		}
 	}
-	return (coun_tokens);
+	free(free_list);
 }
