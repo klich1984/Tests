c5668c9529aed683fe098d77baccd8b279bca0f7
klich1984 <2239@holbertonschool.com>
2021-01-04 13:01:23 -0500

    00 | add file | example of exeptions0

---
+++ python/exceptions/example.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+while True:
+    try:
+        x = int(input("Please enter a number: "))
+        break
+    except ValueError:
+        print("Oops!  That was no valid number.  Try again...")
