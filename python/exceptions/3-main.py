ce78d99dada776b0392032d4cd2160a9972956be
klich1984 <2239@holbertonschool.com>
2021-01-04 18:53:49 -0500

    01 | add file | code running full

--- python/exceptions/3-main.py
+++ python/exceptions/3-main.py
@@ -4,8 +4,6 @@ def safe_print_division(a, b):
     try:
         result = a / b
     except ZeroDivisionError:
-        print("No puedes dividir por cero")
-        print()
         result = None
     finally:
         return result
