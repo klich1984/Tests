9e02c13bad440afab34191193b6deb90c67880f0
klich1984 <2239@holbertonschool.com>
2021-01-04 20:03:59 -0500

    00 | add file | code running task 5 Raise exception

---
+++ python/exceptions/5-main.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+#raise_exception = __import__('5-raise_exception').raise_exception
+def raise_exception():
+    try:
+        raise TypeError("Hola")
+    except TypeError:
+        raise
+
+try:
+    raise_exception()
+except TypeError as te:
+    print("Exception raised")
