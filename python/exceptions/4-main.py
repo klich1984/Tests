05232a8563cef80a7cf8c84745357e43e935b65b
klich1984 <2239@holbertonschool.com>
2021-01-04 19:39:39 -0500

    00 | code fix | code running Special cases

--- python/exceptions/4-main.py
+++ python/exceptions/4-main.py
@@ -7,7 +7,15 @@ def list_division(my_list_1, my_list_2, list_length):
     while idx < list_length:
         try:
             new_list.append(my_list_1[idx]/my_list_2[idx])
-        except (ZeroDivisionError, TypeError, IndexError):
+        except ZeroDivisionError:
+            print("division by 0")
+            new_list.append(0)
+            pass
+        except TypeError:
+            print("wrong type")
+            new_list.append(0)
+        except IndexError:
+            print("out of range")
             new_list.append(0)
         finally:
             idx += 1
