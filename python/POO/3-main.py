ed6d71462ee5c79aa7237159045206ae6db759dd
klich1984 <2239@holbertonschool.com>
2021-01-05 20:34:57 -0500

    00 | add file | code running output good

---
+++ python/POO/3-main.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+#Square = __import__('3-square').Square
+class Square:
+    __size = 0
+
+    def __init__(self, size=0):
+        if type(size) is not int:
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise TypeError("size must be >= 0")
+        else:
+            self.__size = size
+
+    def area(self):
+        return self.__size * self.__size
+
+my_square_1 = Square(3)
+print("Area: {}".format(my_square_1.area()))
+
+try:
+    print(my_square_1.size)
+except Exception as e:
+    print(e)
+
+try:
+    print(my_square_1.__size)
+except Exception as e:
+    print(e)
+
+my_square_2 = Square(5)
+print("Area: {}".format(my_square_2.area()))
