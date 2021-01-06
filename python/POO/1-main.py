18e14ad49e655916c1068c7e1873494265b5fd67
klich1984 <2239@holbertonschool.com>
2021-01-05 19:22:37 -0500

    00 | add file | code running output good

---
+++ python/POO/1-main.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+#Square = __import__('1-square').Square
+class Square:
+    __size = 0
+
+    def __init__(self, new_size):
+        self.__size = new_size
+
+
+my_square = Square(3)
+print(type(my_square))
+print(my_square.__dict__)
+
+try:
+    print(my_square.size)
+except Exception as e:
+    print(e)
+
+try:
+    print(my_square.__size)
+except Exception as e:
+    print(e)
