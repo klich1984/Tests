c01712bf2ea9bc3f2b6859897eb120f823da90a5
klich1984 <2239@holbertonschool.com>
2021-01-05 22:09:03 -0500

    00 | add file | code running output good

---
+++ python/POO/4-main.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+#Square = __import__('4-square').Square
+class Square:
+    __size = 0
+
+    def __init__(self, size=0):
+        self.__size = size
+
+    def area(self):
+        return self.__size * self.__size
+
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        if type(value) is not int:
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise TypeError("size must be >= 0")
+        else:
+            self.__size = value
+
+
+my_square = Square(89)
+print("Area: {} for size: {}".format(my_square.area(), my_square.size))
+
+my_square.size = 3
+print("Area: {} for size: {}".format(my_square.area(), my_square.size))
+
+try:
+    my_square.size = "5 feet"
+    print("Area: {} for size: {}".format(my_square.area(), my_square.size))
+except Exception as e:
+    print(e)
