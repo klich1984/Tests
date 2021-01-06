4ffd2d25560d0b48a30a7998e7a8891f75ad1322
klich1984 <2239@holbertonschool.com>
2021-01-06 16:21:30 -0500

    01 | code fix | text function isinstance

--- python/POO/herenciasuper.py
+++ python/POO/herenciasuper.py
@@ -1,3 +1,9 @@
+"""Princiio de sustitucion
+es siempre un/a para verificar si esta bien diseñado
+para esto usamos isinsta
+isinsta devuelve True si pertenece o False si no
+Una empleado es un persona
+"""
 class Persona():
 
     def __init__(self, nombre, edad, lugar_residencia):
@@ -12,21 +18,32 @@ class Persona():
 
 class Empleado(Persona):
 
-    def __init__(self, salario, antiguedad):
+    def __init__(self, salario, antiguedad, nombre_empleado, edad_empleado, residencia_empleado):
 
-        super().__init__("Carlos", 37, "colombia")
+        super().__init__(nombre_empleado, edad_empleado, residencia_empleado)
 
         self.salario = salario
         self.antiguedad = antiguedad
+
+    def descripcion(self):
+
+        super().descripcion()
+
+        print("Salario:", self.salario, "Antiguedad:", self.antiguedad)
 
 print("------------Antonio-------------")
 
 Antonio = Persona("Antonio", 55, "España")
 
 Antonio.descripcion()
+print(isinstance(Antonio, Empleado))
+print(isinstance(Antonio, Persona))
 
 print("------------Carlos-------------")
 
-Carlos = Empleado(1500, 15)
+Carlos = Empleado(1500, 15, "Carlos", 37, "Colombia")
 
 Carlos.descripcion()
+
+print(isinstance(Carlos, Empleado))
+print(isinstance(Carlos, Persona))
