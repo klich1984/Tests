b93e0f4b9a18494aa00ee0518df34fee8104c54e
klich1984 <2239@holbertonschool.com>
2021-01-08 12:27:14 -0500

    01 | add file | whith text intermediate

--- python/Doctest/example_doctest.py
+++ python/Doctest/example_doctest.py
@@ -1,9 +1,15 @@
-#example_doctest.py
+#doctest_simple_with_docs.py
 
 def my_function(a, b):
-    """
+    """Returns a * b.
+
+    Works whith numbers:
+
     >>> my_function(2, 3)
     6
+
+    and strings:
+
     >>> my_function('a', 3)
     'aaa'
     """
