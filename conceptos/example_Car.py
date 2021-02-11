a72843e86c8e3bb2b9a30d9535c1217c5a11f1f1
klich1984 <2239@holbertonschool.com>
2021-02-11 12:11:25 -0500

    02 | code update | atribute no public

--- conceptos/example_Car.py
+++ conceptos/example_Car.py
@@ -6,6 +6,7 @@ class Car:
         self.color = color
         self.model = model
         self.year = year
+        self._cupholders = 6
 
 my_car = Car('Yellow', 'beetle', 1967)
 my_new_car = Car('red', 'corvette', 1999)
@@ -17,9 +18,12 @@ print(f'wheels class: {Car.wheels}')
 # modifico el atributo de la instancia my_car no se modifica la de la clase
 my_car.wheels = 4
 my_new_car.wheels = 8
+Car.wheels = 20
 print(f'wheels my_car: {my_car.wheels}')
 print(f'wheels class: {Car.wheels}')
 print(f'wheels my_new_car: {my_new_car.wheels}')
+#accediento al atributo NO PUBLICO _cupholders
+print(f'It has {my_car._cupholders} cupholders.')
 
 
 
