b03d351bda3f5fe31d7c5389f45ae6116b191356
klich1984 <2239@holbertonschool.com>
2021-02-12 12:37:17 -0500

    00 | add file | example of jsnos to string

---
+++ examples_json/3-to_json_string.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python3
+""" funcion que devuelve la representacion en json de un objeto(string) """
+import json
+
+
+def to_json_string(my_obj):
+    """ serializar(de pyton a json) el objeto pasado
+
+    Args:
+        my_obj (objeto): Objeto a serializar
+    """
+    x = json.dumps(my_obj)
+    return x
+
+my_list = [1, 2, 3]
+s_my_list = to_json_string(my_list)
+print(s_my_list)
+print(type(s_my_list))
+
+my_dict = {
+    'id': 12,
+    'name': "John",
+    'places': [ "San Francisco", "Tokyo" ],
+    'is_active': True,
+    'info': {
+        'age': 36,
+        'average': 3.14
+    }
+}
+s_my_dict = to_json_string(my_dict)
+print(s_my_dict)
+print(type(s_my_dict))
+
+try:
+    my_set = { 132, 3 }
+    s_my_set = to_json_string(my_set)
+    print(s_my_set)
+    print(type(s_my_set))
+except Exception as e:
+    print("[{}] {}".format(e.__class__.__name__, e))
