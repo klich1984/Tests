660de1356561d61351fed929cf77c234111310c2
klich1984 <2239@holbertonschool.com>
2021-02-12 12:55:10 -0500

    00 | add file | example of jdon to python

---
+++ examples_json/4-from_json_string.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+"""  Funcion que convierte un json a objeto """
+import json
+
+def from_json_string(my_str):
+    """Convierte un json(string) en un objeto de pyton
+
+    Args:
+        my_str (json string): Json string
+    Return: objeto de pyton
+    """
+    x = json.loads(my_str)
+    return x
+
+s_my_list = "[1, 2, 3]"
+my_list = from_json_string(s_my_list)
+print(my_list)
+print(type(my_list))
+
+s_my_dict = """
+{"is_active": true, "info": {"age": 36, "average": 3.14}, 
+"id": 12, "name": "John", "places": ["San Francisco", "Tokyo"]}
+"""
+my_dict = from_json_string(s_my_dict)
+print(my_dict)
+print(type(my_dict))
+
+try:
+    s_my_dict = """
+    {"is_active": true, 12 }
+    """
+    my_dict = from_json_string(s_my_dict)
+    print(my_dict)
+    print(type(my_dict))
+except Exception as e:
+    print("[{}] {}".format(e.__class__.__name__, e))
